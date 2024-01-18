53eae4e676f42edc614f828b3023f3348e9e0fa9
Roopesh Verma <roopeshv.30@gmail.com>
2024-01-18 16:44:49 +0530

    Add grid class

--- HeaderFiles/Grid.h
+++ HeaderFiles/Grid.h
@@ -1,4 +1,17 @@
 #ifndef GRID_H
 #define GRID_H
 
+#define N 9
+
+class Grid
+{
+private:
+    char grid[N][N] = {'0'};
+public:
+    void Print();
+    void SetupMines();
+    void SetCellValues();
+    void GetInput(int row, int col);
+};
+
 #endif
