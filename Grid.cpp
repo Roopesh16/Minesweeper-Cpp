a37338d0d9ae9de239db08d3858d43fae53a60a1
Roopesh Verma <roopeshv.30@gmail.com>
2024-01-19 16:38:31 +0530

    Add setup mine function

--- Grid.cpp
+++ Grid.cpp
@@ -2,14 +2,42 @@
 #include <iostream>
 using namespace std;
 
+bool isFirst = true;
+
+void Grid::GetInput(int row, int col)
+{
+    if (isFirst)
+    {
+        SetupMines(row, col);
+        SetCellValues();
+    }
+}
+
+void Grid::SetupMines(int row, int col)
+{
+    int time = 10;
+    int x, y;
+    while (time > 0)
+    {
+        x = (rand() % (N + 1));
+        y = (rand() % (N + 1));
+
+        while ((x == row && y == col) || grid[x][y].GetCellType() == MINE)
+        {
+            x = (rand() % (N + 1));
+            y = (rand() % (N + 1));
+        }
+
+        grid[x][y].SetCellType = MINE;
+        time--;
+    }
+}
+
 void Grid::SetCellValues()
 {
-    if (this.cellType == MINE)
-        return;
-
-    for (int i = row - 1; i <= row + 1;i++)
+    for (int i = row - 1; i <= row + 1; i++)
     {
-        for (int j = col - 1; i <= col + 1;j++)
+        for (int j = col - 1; i <= col + 1; j++)
         {
             grid[i][j].SetCellValue();
         }
