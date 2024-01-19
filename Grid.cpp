c28398ef2d94f8c36b701fb7c197a2244673968f
Roopesh Verma <roopeshv.30@gmail.com>
2024-01-19 18:47:59 +0530

    Add Set Cell value

--- Grid.cpp
+++ Grid.cpp
@@ -1,4 +1,5 @@
 #include "HeaderFiles/Grid.h"
+#include "HeaderFiles/Cell.h"
 #include <iostream>
 using namespace std;
 
@@ -12,14 +13,14 @@ void Grid::GetInput(int row, int col)
         SetCellValues();
     }
 
-    if(grid[row][col]->GetCellType() == EMPTY)
-        OpenEmptyGrid(row,col);
-    else if(grid[row][col]->GetCellType() == NUMBER)
+    if (grid[row][col]->GetCellType() == EMPTY)
+        OpenEmptyGrid(row, col);
+    else if (grid[row][col]->GetCellType() == NUMBER)
         grid[row][col]->SetCellState(OPEN);
     else
     {
         grid[row][col]->SetCellState(OPEN);
-        //SetGameState(OVER);
+        // SetGameState(OVER);
     }
 
     PrintGrid();
@@ -34,42 +35,41 @@ void Grid::SetupMines(int row, int col)
         x = (rand() % (N + 1));
         y = (rand() % (N + 1));
 
-        while ((x == row && y == col) || grid[x][y].GetCellType() == MINE)
+        while ((x == row && y == col) || grid[x][y]->GetCellType() == MINE)
         {
             x = (rand() % (N + 1));
             y = (rand() % (N + 1));
         }
 
-        grid[x][y].SetCellType = MINE;
+        grid[x][y]->SetCellType(MINE);
         time--;
     }
 }
 
 void Grid::SetCellValues()
 {
-    for (int i = row - 1; i <= row + 1; i++)
+    for (int i = 0; i < N; i++)
     {
-        for (int j = col - 1; i <= col + 1; j++)
+        for (int j = 0; i < N; j++)
         {
-            grid[i][j].SetCellValue();
+            grid[i][j]->SetCellValue();
         }
     }
 }
 
-void Grid::OpenEmptyGrid(int row,int col)
+void Grid::OpenEmptyGrid(int row, int col)
 {
-    
 }
 
 void Grid::PrintGrid()
 {
-    for (int i = 0; i < N;i++)
+    for (int i = 0; i < N; i++)
     {
-        for (int j = 0; j < N;j++)
+        for (int j = 0; j < N; j++)
         {
             cout << grid[i][j]->GetCellValue() << " ";
         }
 
-        endl;
+        cout << endl;
     }
 }
