3be482e14372cce4de4c72686b0f933df0afc438
Roopesh Verma <roopeshv.30@gmail.com>
2024-01-19 21:59:43 +0530

    Setup main

--- main.cpp
+++ main.cpp
@@ -1,10 +1,69 @@
 #include "HeaderFiles/Grid.h"
 #include "HeaderFiles/Cell.h"
 #include <iostream>
+#include <iomanip>
 using namespace std;
+
+std::ostream &bold_on(std::ostream &os) { return os << "\e[1m"; }
+
+std::ostream &bold_off(std::ostream &os) { return os << "\e[0m"; }
+
+enum State
+{
+    PLAY,
+    WIN,
+    OVER
+};
+
+State state;
+
+State GetState()
+{
+    return state;
+}
+
+void SetState(State newState)
+{
+    state = newState;
+}
+
+void BasicInfo()
+{
+    cout << bold_on << setw(45) << "MINESWEEPER\n\n"
+         << bold_off;
+    cout << "Minesweeper is a classic game which everyone has played but no one knos the rule of! So here are the rules - \n";
+    cout << "1. You can input between 0-9 as row and column.\n";
+    cout << "2. When the grid opens, you will see numbers on the cell. This number represents the number of mine adjacent to current cell\n";
+    cout << "3. If you open a mine, then GAME OVER.\n";
+    cout << "4. If you avoid all the mines, then you win\n";
+    cout << "\n";
+}
 
 int main()
 {
+    BasicInfo();
+
+    SetState(PLAY);
+
     Grid *grid = new Grid();
-    grid->GetInput(0, 1);
+
+    while (GetState() != OVER || GetState() != WIN)
+    {
+        int row, col;
+        cout << "Enter Row : ";
+        cin >> row;
+        cout << "Enter Column : ";
+        cin >> col;
+
+        grid->GetInput(row, col);
+    }
+
+    if(GetState() == WIN)
+    {
+        cout << bold_on << "GAME WON!!!" << bold_off;
+    }
+    else
+    {
+        cout << bold_on << "GAME OVER!!!" << bold_off;
+    }
 }
